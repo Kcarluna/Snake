1262f7ffd371a042e008025988f50b00e21cafc5
Jacob Luna <luna@Attest.local>
2022-07-18 22:15:09 -0700

    abstract update and render

--- main.cpp
+++ main.cpp
@@ -18,6 +18,77 @@ static int LBOUND_HEIGHT = (HEIGHT / 2) - (MAP_HEIGHT / 2);
 static int UBOUND_WIDTH = (WIDTH / 2) + (MAP_WIDTH / 2); 
 static int UBOUND_HEIGHT = (HEIGHT / 2) + (MAP_HEIGHT / 2); 
 
+bool QUIT = false, PAUSED = false;
+SDL_Rect MAP = {LBOUND_WIDTH, LBOUND_HEIGHT, MAP_WIDTH, MAP_HEIGHT};
+Snake SNAKE(0, 0);
+void update(SDL_Event *event) {
+while(SDL_PollEvent(event)) {
+			switch(event->type) {
+				case SDL_QUIT:
+					QUIT = true;
+					break;
+				case SDL_WINDOWEVENT:
+					switch(event->window.event) {
+						case SDL_WINDOWEVENT_RESIZED:
+							WIDTH = event->window.data1;
+							HEIGHT = event->window.data2;
+							LBOUND_WIDTH = (WIDTH / 2) - (MAP_WIDTH / 2); 
+							LBOUND_HEIGHT = (HEIGHT / 2) - (MAP_HEIGHT / 2); 
+							UBOUND_WIDTH = (WIDTH / 2) + (MAP_WIDTH / 2); 
+							UBOUND_HEIGHT = (HEIGHT / 2) + (MAP_HEIGHT / 2); 
+
+							MAP = {LBOUND_WIDTH, LBOUND_HEIGHT, MAP_WIDTH, MAP_HEIGHT};
+
+							SNAKE.set_x(LBOUND_WIDTH);
+							SNAKE.set_y(LBOUND_HEIGHT);
+							break;
+						default:
+							break;
+					}
+				case SDL_KEYDOWN:
+					switch(event->key.keysym.sym) {
+						case SDLK_w:
+							SNAKE.set_direction(0, -1);
+							break;
+						case SDLK_a:
+							SNAKE.set_direction(-1, 0);
+							break;
+						case SDLK_s:
+							SNAKE.set_direction(0, 1);
+							break;
+						case SDLK_d:
+							SNAKE.set_direction(1, 0);
+							break;
+						case SDLK_SPACE:
+							PAUSED = !PAUSED;
+							break;
+						default:
+							break;
+					}
+				default:
+					break;
+			}
+		}
+}
+
+void render(SDL_Renderer *renderer) {
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, palette[0].r, palette[0].g, palette[0].b, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, palette[1].r, palette[1].g, palette[1].b, SDL_ALPHA_OPAQUE);
+		SDL_RenderFillRect(renderer, &MAP);
+
+		SDL_SetRenderDrawColor(renderer, palette[2].r, palette[2].g, palette[2].b, SDL_ALPHA_OPAQUE);
+		for (int x = LBOUND_WIDTH; x <= UBOUND_WIDTH; x += SQUARE) {
+			SDL_RenderDrawLine(renderer, x, LBOUND_HEIGHT, x, UBOUND_HEIGHT);
+		}
+		for (int y = LBOUND_HEIGHT; y <= UBOUND_HEIGHT; y += SQUARE) {
+			SDL_RenderDrawLine(renderer, LBOUND_WIDTH, y, UBOUND_WIDTH, y);
+		}
+
+		SNAKE.render(renderer, palette[3]);
+		SDL_RenderPresent(renderer);
+}
 
 int main() {
 	SDL_Window *window;
@@ -33,81 +104,22 @@ int main() {
 	}
 	SDL_SetWindowTitle(window, "SNAKE");
 
-	SDL_Rect map = {LBOUND_WIDTH, LBOUND_HEIGHT, MAP_WIDTH, MAP_HEIGHT};
-	Snake snake(0, 0);
+	while (!QUIT) {
+		update(&event);
 
-	bool quit = false;
-	while (!quit) {
-		while(SDL_PollEvent(&event)) {
-			switch(event.type) {
-				case SDL_QUIT:
-					quit = true;
-					break;
-				case SDL_WINDOWEVENT:
-					switch(event.window.event) {
-						case SDL_WINDOWEVENT_RESIZED:
-							WIDTH = event.window.data1;
-							HEIGHT = event.window.data2;
-							LBOUND_WIDTH = (WIDTH / 2) - (MAP_WIDTH / 2); 
-							LBOUND_HEIGHT = (HEIGHT / 2) - (MAP_HEIGHT / 2); 
-							UBOUND_WIDTH = (WIDTH / 2) + (MAP_WIDTH / 2); 
-							UBOUND_HEIGHT = (HEIGHT / 2) + (MAP_HEIGHT / 2); 
-
-							map = {LBOUND_WIDTH, LBOUND_HEIGHT, MAP_WIDTH, MAP_HEIGHT};
-
-							snake.set_x(LBOUND_WIDTH);
-							snake.set_y(LBOUND_HEIGHT);
-							break;
-						default:
-							break;
-					}
-				case SDL_KEYDOWN:
-					switch(event.key.keysym.sym) {
-						case SDLK_w:
-							snake.set_direction(0, -1);
-							break;
-						case SDLK_a:
-							snake.set_direction(-1, 0);
-							break;
-						case SDLK_s:
-							snake.set_direction(0, 1);
-							break;
-						case SDLK_d:
-							snake.set_direction(1, 0);
-							break;
-						default:
-							break;
-					}
-				default:
-					break;
-			}
+		if (!SNAKE.get_foodExists()) {
+			SNAKE.generate_food(LBOUND_WIDTH / SQUARE, LBOUND_HEIGHT / SQUARE, (UBOUND_WIDTH - SQUARE) / SQUARE, (UBOUND_HEIGHT - SQUARE) / SQUARE);
 		}
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, palette[0].r, palette[0].g, palette[0].b, SDL_ALPHA_OPAQUE);
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, palette[1].r, palette[1].g, palette[1].b, SDL_ALPHA_OPAQUE);
-		SDL_RenderFillRect(renderer, &map);
-
-		SDL_SetRenderDrawColor(renderer, palette[2].r, palette[2].g, palette[2].b, SDL_ALPHA_OPAQUE);
-		for (int x = LBOUND_WIDTH; x <= UBOUND_WIDTH; x += SQUARE) {
-			SDL_RenderDrawLine(renderer, x, LBOUND_HEIGHT, x, UBOUND_HEIGHT);
-		}
-		for (int y = LBOUND_HEIGHT; y <= UBOUND_HEIGHT; y += SQUARE) {
-			SDL_RenderDrawLine(renderer, LBOUND_WIDTH, y, UBOUND_WIDTH, y);
+		SNAKE.eat();
+		if (!PAUSED) {
+			SNAKE.update();
 		}
 
-		if (!snake.get_foodExists()) {
-			snake.generate_food(LBOUND_WIDTH / SQUARE, LBOUND_HEIGHT / SQUARE, (UBOUND_WIDTH - SQUARE) / SQUARE, (UBOUND_HEIGHT - SQUARE) / SQUARE);
-		}
-		snake.eat();
-		snake.update();
-		snake.render(renderer, palette[3]);
-		SDL_RenderPresent(renderer);
+		render(renderer);
 		SDL_Delay(FPS);
 	}
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
 	return 0;
 }
